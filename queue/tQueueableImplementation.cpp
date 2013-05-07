5f723617f908d70369737fb3cc812ab50cee8da6
Tobias Föhst <foehst@informatik.uni-kl.de>
2013-05-07 02:26:03 +0200

    Updated file info

--- queue/tQueueableImplementation.cpp
+++ queue/tQueueableImplementation.cpp
@@ -19,7 +19,7 @@
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 //
 //----------------------------------------------------------------------
-/*!\file    rrlib/concurrent_containers/queue/tQueuableImplementation.cpp
+/*!\file    rrlib/concurrent_containers/queue/tQueueableImplementation.cpp
  *
  * \author  Max Reichardt
  *
